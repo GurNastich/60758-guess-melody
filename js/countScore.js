64992ff09d617423b3db0fd51477dafb792e3697
Anastasia Gurevich <anastasia_gurevich@epam.com>
2018-09-13 15:52:30 +0300

    use reduce

--- js/countScore.js
+++ js/countScore.js
@@ -4,17 +4,17 @@ const countScore = (answers) => {
   if (answers.length < 10) {
     score = -1;
   } else {
-    for (const answer of answers) {
-      if (answer.correct && answer.time < 30) {
-        score += 2;
-      }
-      if (answer.correct && answer.time >= 30) {
-        score += 1;
-      }
+    score = answers.reduce((acc, answer) => {
       if (!answer.correct) {
-        score -= 2;
+        return acc - 2;
       }
-    }
+      if (answer.time < 30) {
+        return acc + 2;
+      }
+
+      return acc + 1;
+
+    }, score);
   }
   return score;
 };
